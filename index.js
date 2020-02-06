1231ba184b618cf7c1d996c7e7349a25c5bf752d
DuyLuu90 <duyluu90@hotmail.com>
2020-02-05 19:49:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,40 +2,40 @@ $(function() {
 //enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
     $("#js-shopping-list-form").submit(event => {
         event.preventDefault();
+      //find the descendant of the selected element that has matched name, and return its value
         const product= $(event.currentTarget).find('input[name="shopping-list-entry"]').val();
-    }); //get the value
-
-    console.log(product);
+     //add(append) the new "product" to ".shopping-list"
+       $(".shopping-list").append(
+         `<li>
+            <span class="shopping-item">${product}</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>`);
+   });
 
 //create "event delegation" for ."shopping-list" (applied for all newly added "product")
 
     //check and uncheck items on the list by clicking the "Check" button("shopping-item"=>"shopping-item shopping-item__checked")
     $('.shopping-list').on('click',"shopping-item-controls", event=>{
-        event.preventDefault();
-        $('span').toggleClass("shopping-item__checked");
+        $(event.target).closest('.shopping-item').toggleClass("shopping-item__checked");
         //if "shopping-item__checked" is already there, it will be removed
     });
-    //permanently remove items from the list
-    $('.shopping-list').on('click',"shopping-item-delete", event=>{
-        event.preventDefault();
+
+
+    //select class ".shopping-list" element, listen to "click" on class".shopping-item-delete", find the nearest ancestor of the selected item that is <li>, then permanently remove that <i> from the list
+    $('.shopping-list').on('click',".shopping-item-delete", event=>{
         $(event.target).closest('li').remove();
-    });
-//add(append) the new "product" to ".shopping-list"
-    $(".shopping-list").append(`<li>
-    <span class="shopping-item">${product}</span>
-    <div class="shopping-item-controls">
-      <button class="shopping-item-toggle">
-        <span class="button-label">check</span>
-      </button>
-      <button class="shopping-item-delete">
-        <span class="button-label">delete</span>
-      </button>
-    </div>
-  </li>`);
+    });    
 });
 
 //submit()
-//preventDefault()
+//preventDefault(): stop the default submission behaviou for form
 //toggleClass(): add/remove one/more classes from each matched element (add when the class is NOT present, and remove when it is)
 //remove(): remove data/element
 //detach(): remove element but NOT data
